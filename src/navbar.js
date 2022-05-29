9efe6fd77ab522b68897f0821a4615f11549dae7
Mariam Hindawi <mariamashraf587@yahoo.com>
2022-05-30 01:40:14 +0200

    Edited desktop and mobile navbar

--- src/navbar.js
+++ src/navbar.js
@@ -1,34 +1,59 @@
-import React from 'react'
+import React, {useState} from 'react'
 import logo from './images/logo.png'
-import {FaSearch } from "react-icons/fa";
+import {FaBars, FaSearch } from "react-icons/fa";
 
 export default function navbar() {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const [display, setDisplay] = useState('nav-items-mobile show');
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const [isActive, setActive] = useState("false");
+  const handleToggle = () => {
+    setActive(!isActive);
+    if(isActive){
+      setDisplay('nav-items-mobile not-show');
+    }
+    else{
+        setDisplay('nav-items-mobile show');
+    }
+  };
   return (
-    <nav  className ='nav-container' role="navigation">
-      <div className='brand-logo'>
-      <img src={logo} alt=''></img>
-      </div>
-  
-<div className='nav-items-container'>
-<ul class="nav-list">
-    <li class="nav-item">360 Planner</li>
-    <li class="nav-item">Her</li>
-    <li class="nav-item">Him</li>
-    <li class="nav-item">The Wedding</li>
-    <li class="nav-item">Vendors</li>
-    <li class="nav-item">Gallery</li>
-    <li class="nav-item">Ideas&More</li>
-    <li class="nav-item">360 Planner</li>
-    <li class="nav-item">Take a tour</li>
-  </ul>
-
-  <div className='search-container'>
-    <div >
-    <FaSearch id="search-icon"></FaSearch>
+<>
+<div className='navbar-desktop'>
+    <div className='nav-logo'>
+      <img src={logo} alt="logo"></img>
     </div>
-  <input type="text" autocomplete="off" id="search-bar" class="" placeholder="Search"></input>
-  </div>
+   <div className='nav-container'>
+    <div className='nav-list-container'>
+        <ul class="nav-list">
+          <li class="nav-item">360 Planner</li>
+          <li class="nav-item">Her</li>
+          <li class="nav-item">Him</li>
+          <li class="nav-item">The Wedding</li>
+          <li class="nav-item">Vendors</li>
+          <li class="nav-item">Gallery</li>
+          <li class="nav-item">Ideas&More</li>
+          <li class="nav-item">Take a tour</li>
+        </ul>
+      </div>
+      <div className='search-container'>
+        <FaSearch id="search-icon"></FaSearch>
+        <input type="text" autocomplete="off" id="search-bar" class="" placeholder="Search"></input>
+      </div>
+   </div>
 </div>
-</nav>
+<div className='navbar-mobile'>
+  <div className='nav-logo'>
+      <img src={logo} alt="logo"></img>
+    </div>
+    <div className='search-container'>
+        <FaSearch id="search-icon"></FaSearch>
+        <input type="text" autocomplete="off" id="search-bar" class="" placeholder="Search"></input>
+      </div>
+    <div className='bars' onClick={handleToggle}>
+      <FaBars className='bars'></FaBars>
+    </div>
+</div>
+</>
+
   )
 }
